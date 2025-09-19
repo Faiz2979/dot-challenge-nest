dcf155a944f98ffefbfbfc5c73b6a227e11932ce
Faiz2979 <faizannabil877@gmail.com>
2025-09-19 20:07:48 +0700

    remove unessesary module

--- src/users/users.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class UsersService {
-
-    profile() {
-        return { message: 'This action returns user profile' };
-    }
-}
