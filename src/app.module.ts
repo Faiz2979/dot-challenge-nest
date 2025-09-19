77d2a5089d23c80ec5c0aa7746f1aed364483d90
Faiz2979 <faizannabil877@gmail.com>
2025-09-19 20:07:48 +0700

    chore(project): clean up unused module in project structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthController } from './auth/auth.controller';
 import { AuthModule } from './auth/auth.module';
-import { PostModule } from './post/post.module';
-import { UsersModule } from './users/users.module';
-import { PrismaService } from './prisma/prisma.service';
-import { PrismaModule } from './prisma/prisma.module';
 import { PostController } from './post/post.controller';
+import { PostModule } from './post/post.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  imports: [PostModule, UsersModule, AuthModule, PrismaModule],
+  imports: [PostModule, AuthModule, PrismaModule],
   controllers: [AppController, AuthController, PostController],
   providers: [AppService, PrismaService],
 })
