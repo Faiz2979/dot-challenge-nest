3b8fd29fbcb4ee2147422bd2438a97c15a8b99da
Faiz2979 <faizannabil877@gmail.com>
2025-09-19 20:01:28 +0700

    feat(post): add, update, and delete post functionality

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,5 +1,5 @@
-import { Body, Controller, Get, Headers, Param, Post, Query, UnauthorizedException } from '@nestjs/common';
-import { CreatePostDtoPayload, CreatePostDtoResponse } from 'src/dtos/post.dtos';
+import { Body, Controller, Delete, Get, Headers, Param, Patch, Post, Query, UnauthorizedException } from '@nestjs/common';
+import { CreatePostDtoPayload, CreatePostDtoResponse, UpdatePostDtoPayload } from 'src/dtos/post.dtos';
 import { PostService } from './post.service';
 
 @Controller('posts')
@@ -32,5 +32,41 @@ export class PostController {
             page: page ? parseInt(page, 10) : 1,
             limit: limit ? parseInt(limit, 10) : 10,
         });
+    }
+
+    @Get()
+    async getAllPosts(
+        @Query('page') page?: string,
+        @Query('limit') limit?: string,
+    ) {
+        return this.postService.getAllPosts({
+            page: page ? parseInt(page, 10) : 1,
+            limit: limit ? parseInt(limit, 10) : 10,
+        });
+    }
+
+    @Delete(':postId')
+    async deletePost(@Param('postId') postId: string, @Headers('authorization') authHeader: string) {
+        if (!authHeader || !authHeader.startsWith('Bearer ')) {
+            throw new UnauthorizedException('Missing or invalid authorization header');
+        }
+
+        const token = authHeader.replace('Bearer ', '').trim();
+
+        return this.postService.deletePost(postId, token);
+    }
+
+    @Patch(':postId')
+    async updatePost(
+        @Param('postId') postId: string,
+        @Body() payload: UpdatePostDtoPayload,
+        @Headers('authorization') authHeader: string,
+    ) {
+        if (!authHeader || !authHeader.startsWith('Bearer ')) {
+            throw new UnauthorizedException('Missing or invalid authorization header');
+        }
+        const token = authHeader.replace('Bearer ', '').trim();
+
+        return this.postService.updatePost(postId, payload, token);
     }
 }
